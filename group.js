987bb20e1fa397bbb8dc397d8666262c6697dcdd
David Beckley <beckl.ds@gmail.com>
2015-08-02 21:41:03 -0700

    Work on genetic algorithm

--- group.js
+++ group.js
@@ -6,12 +6,12 @@ var students = JSON.parse(fs.readFileSync('random.dat', 'utf8'));
 var n = students.length;
 
 // number of students in group
-var group_size = 5;
+var group_size = 2;
 
 // number of groups
 var k = n / group_size;
 
-var costs = [
+var weights = [
   1, // Refuse to work with based on technical ability
   1, // Refuse to work with based on intrapersonal issues
   -1 // Would particularly enjoy working with
@@ -28,7 +28,7 @@ var shuffle = function(arr) {
     //swap(arr, arr[randIndex], arr[i]);
     i--;
   }
-}
+};
 
 var randomGrouping = function() {
   var group = [];
@@ -38,7 +38,7 @@ var randomGrouping = function() {
 
   shuffle(group);
   return group;
-}
+};
 
 var studentCost = function(student, group) {
   var sum = 0;
@@ -48,39 +48,109 @@ var studentCost = function(student, group) {
   }
 
   for (var i = 0; i < student.opinions.length; i++) {
-    for (var j = 0; j < costs.length; j++) {
+    for (var j = 0; j < weights.length; j++) {
       if (group[i]) {
-        sum += costs[j] * student.opinions[i][j];
+        sum += weights[j] * student.opinions[i][j];
       }
     }
   }
 
   return sum;
-}
+};
 
 var cost = function(grouping) {
   var sum = 0;
 
   if (grouping.length !== n) throw new Error('You terrible person');
   for (var i = 0; i < n; i += group_size) {
-    var group = {};
+    var inGroup = {};
     for (var j = i; j < i + group_size; j++) {
-      group[grouping[j]] = true;
+      inGroup[grouping[j]] = true;
     }
 
-    for (var j = i; j < i + group_size; j++) {
-      sum += studentCost(students[grouping[j]], group);
+    for (j = i; j < i + group_size; j++) {
+      var currentStudent = students[grouping[j]];
+      var costOfStudent = studentCost(currentStudent, inGroup);
+      sum += costOfStudent;
     }
   }
 
   return sum;
+};
+
+var fitness = function(grouping) {
+  // The minimum cost is -group_size * k
+  // which would signify a perfect solution
+  return 1 / (cost(grouping) + group_size * k);
 }
 
 // genetic algorithm
+var poolLength = 20;
+
+var crossoverRate = 0.7;
+var mutationRate = 0.001;
+
 var pool = [];
+
+for (var i = 0; i < poolLength; i++) {
+  pool.push(randomGrouping());
+}
 
 /*console.log(randomGrouping().map(function(i) {
   return students[i].name;
 }));*/
 
-console.log(cost(randomGrouping()));
+var roulette = function(values) {
+  var total = values.reduce(function(memo, g) {
+    return memo + g;
+  }, 0);
+
+  var random = Math.random() * total;
+
+  var soFar = 0;
+  for (var i = 0; i < values.length; i++) {
+    soFar += values[i];
+
+    if (soFar >= random) {
+      return i;
+    }
+  }
+
+  return values.length - 1;
+}
+
+// let's just do 10 generations for now
+for (var i = 0; i < 10; i++) {
+  var newPool = [];
+
+  var fitnesses = [];
+
+  var totalFitness = 0;
+
+  for (var j = 0; j < pool.length; j++) {
+    var f = fitness(pool[j]);
+    totalFitness += f;
+    fitnesses.push(f);
+  }
+
+  console.log('Generation', i, 'average fitness:', totalFitness / pool.length);
+
+  while (newPool.length < pool.length) {
+    var p1 = roulette(fitnesses);
+    var p2 = roulette(fitnesses);
+
+    if (Math.random() < crossoverRate) {
+      var child = crossover(p1, p2);
+    } else {
+      var child = p1;
+    }
+
+    if (Math.random() < mutationRate) {
+      mutate(child);
+    }
+
+    newPool.push(child);
+  }
+
+  pool = newPool;
+}
